e1092663c191953fca5d657c0085a0abcc9d24e4
Alex Korovyansky <korovyansk@gmail.com>
2013-05-26 15:19:50 +0700

    split secret constants

---
+++ constants_secret.py
@@ -0,0 +1,4 @@
+#secret information, find it in secret doc - https://docs.google.com/document/d/11uZelaTm_T-JBwkXQPVRKDPtlQDsqWkyYoXzNBR5kn4/edit
+
+GOOGLE_PLUS_API_KEY = 'change_me'
+VKONTAKTE_EKATERINA_LUBIMOVA_TOKEN = 'change_me'
